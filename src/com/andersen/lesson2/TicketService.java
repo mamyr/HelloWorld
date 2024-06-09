7bddc985f1873c6fcd97b200f5395da9fce70ff1
mamyr <kpb_janat@yahoo.com>
2024-06-09 15:46:57 +0500

    get ticket by ID

--- src/com/andersen/lesson2/TicketService.java
+++ src/com/andersen/lesson2/TicketService.java
@@ -14,7 +14,28 @@ public class TicketService {
         for(int i=0; i<10; i++) {
             tickets[i] = new Ticket("000"+i,"qwertyuiop",201,time_now,true,'B',405.45);
         }
+        printTickets(tickets);
+        getTicketByID(tickets, "0000");
+        
         empty_ticket.save_create_time(time_now);
         empty_ticket.save_price(4);
+    }
+
+    public static void printTickets(Ticket[] tickets){
+        for(int i=0; i<tickets.length; i++){
+            System.out.println("ticket.id["+i+"]="+tickets[i].getID());
+        }
+    }
+
+    public static Ticket getTicketByID(Ticket[] tickets, String ID){
+        int foundIndex = 0;
+        for(int i=0; i<tickets.length; i++){
+            if(tickets[i].getID().equals(ID)){
+                System.out.println("ticket.id["+i+"]="+tickets[i].getID());
+                foundIndex = i;
+                break;
+            }
+        }
+        return tickets[foundIndex];
     }
 }
