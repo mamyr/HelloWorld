5c90e1aaa5c607cd201e100b3872428aaabc44e5
mamyr <kpb_janat@yahoo.com>
2024-06-09 15:23:40 +0500

    Single responsibility principle fixed and unnecessary comments deleted

--- src/com/andersen/lesson2/TicketService.java
+++ src/com/andersen/lesson2/TicketService.java
@@ -1,5 +1,7 @@
 package com.andersen.lesson2;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Date;
 
 
@@ -10,7 +12,18 @@ public class TicketService {
         Ticket full_ticket = new Ticket("A001","qwertyuiop",201,time_now,true,'B',405.45);
         Ticket limited_ticket = new Ticket("asdfghjklm", 301, time_now);
 
-        empty_ticket.save_create_time(time_now);
-        empty_ticket.save_price(4);
+        save_create_time(time_now, full_ticket);
+        save_price(4, limited_ticket);
     }
+
+    public static void save_create_time(long time, @org.jetbrains.annotations.NotNull Ticket t){
+        t.setCreat_time(time/1000L);
+        System.out.println("Creation time is saved.");
+    }
+
+    public static void save_price(int price, @NotNull Ticket t){
+        t.setPrice(price);
+        System.out.println("Price is saved.");
+    }
+
 }
