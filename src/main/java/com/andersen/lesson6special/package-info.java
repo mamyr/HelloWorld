9d24b4db733b00e66f2285851dc2740744313dea
mamyr <kpb_janat@yahoo.com>
2024-06-26 00:20:37 +0500

    HW-6 special:java docs

---
+++ src/main/java/com/andersen/lesson6special/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Provides service for the {@code BusTicket} class from the package {@code org.example} to create an instance,
+ * store in a storage list, remove by id from a storage list, get bus ticket by specified id from storage,
+ * get bus tickets by ticket type from storage, get bus tickets by price range.
+ * <p>
+ *     Testing of methods is processed in the main method.
+ * </p>
+ */
+package com.andersen.lesson6special;
