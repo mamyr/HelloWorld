230418c31718ed610b68b3b265caa485f46b948e
mamyr <kpb_janat@yahoo.com>
2024-07-07 10:06:32 +0500

    separated lesson9 from lesson10

--- src/main/java/com/andersen/lesson9/Models/User.java
+++ src/main/java/com/andersen/lesson9/Models/User.java
@@ -4,23 +4,33 @@ import jakarta.persistence.*;
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 @org.hibernate.annotations.NamedQueries({ @org.hibernate.annotations.NamedQuery(name = "User_GetUserById", query = "from User where id = :id"),
         @org.hibernate.annotations.NamedQuery(name = "User_UpdateUserById", query = "Update User set id = :newId where id = :id")})//,
 //        @org.hibernate.annotations.NamedNativeQuery(name = "DeptEmployee_UpdateEmployeeDesignation", query = "call UPDATE_EMPLOYEE_DESIGNATION(:employeeNumber, :newDesignation)", resultClass = DeptEmployee.class) })
 @Entity
-@Table(name = "public.user")
+@Table(name = "public.user", uniqueConstraints = {@UniqueConstraint(columnNames={"id"})})
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="user_seq")
-    @SequenceGenerator(name = "user_seq", sequenceName="user_seq")
+    @SequenceGenerator(name = "user_seq", sequenceName="user_seq",
+            initialValue = 1, allocationSize = 1)
     private Integer id;
     private String name;
     private Timestamp creationDate;
 
     // A user can have many tickets
-    @OneToMany(mappedBy = "user")
-    private List<Ticket> ticketList;
+    @OneToMany(mappedBy = "user", cascade=CascadeType.ALL)
+    @JoinColumn(name="user_id")
+    private Set<Ticket> ticketList;
+
+    public  void setTicketList(Set<Ticket> ticketList){
+        this.ticketList = ticketList;
+    }
+    public Set<Ticket> getTicketList(){
+        return this.ticketList;
+    }
 
     public void setId(Integer id){
         this.id = id;
